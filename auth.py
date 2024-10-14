da3c2d11301d924aeccbe1565dec8ef2070b8742
tlsnwls <tlsdnwls0103@gmail.com>
2024-10-14 22:00:04 +0900

    feat: 구조 변경

--- auth.py
+++ auth.py
@@ -1,12 +1,13 @@
 from dotenv import load_dotenv
 import os
+import random
 from datetime import datetime, timedelta, timezone
-from typing import Optional
-import jwt
+from zoneinfo import ZoneInfo
+from typing import Optional, Union, Any
 from fastapi import Depends, HTTPException, status, APIRouter
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jwt.exceptions import InvalidTokenError
 from pydantic import BaseModel
+from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 
 from schemes import User
@@ -56,17 +57,54 @@ def verify_card_response(response : str):
         raise HTTPException(status_code=400, detail="Invalid response")
 
 
-# SQL :
-# SELECT uuid(session_id) 
-# FROM osmember 
-# WHERE card.uuid = osmember.uuid 
-# if card.uuid == osmember.uuid
-#   data(session_id : uuid) = db.query(OsMember).filter(OsMember.uuid == card.uuid)
-#   return data
-# data(session_id : uuid) => 해싱한 값을 쿼리 파라미터로 담을까?
+# API KEY 생성
+# 생성된 API_KEY를 DB에 INSERT  함
+# 
+@verify_router.get("/v1/api-key")
+def gen_api_key(db : Session = Depends(get_db)):
+    api_key = hex(random.getrandbits(128))
+    try:
+        if api_key not in db:
+            return api_key
+    except:
+        HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="API KEY ISSUED FAIL")
+    return api_key
 
-@verify_router.get("/v1/user/{uuid}")
-def get_user(db: Session = Depends(get_db)):
+# TODO : 인가 코드 발급
+# 1. 카드를 통해 로그인을 시도
+# 2. 정보가 일치하면 인가 코드를 서비스 서버에 제공한다.
+
+# 서비스 서버에서 인가 코드 요청(로그인 시도)했을 시 동작
+@verify_router.post("/v1/authorization_code?api-key={API_KEY}&redirect_uri={redirec_uri}")
+def sup_authrization_code(API_KEY):
+      """
+      API KEY가 일치하고 카드의 UUID와 DB의 UUID가 일치하면
+      인가코드를 줌
+      if user.apikey == server.apikey && card.uuid == db.uuid
+        return authcode(random)
+      """
+
+#  서비스 서버가 인가 코드로 access token을 발급 요청했을 시 동작 
+@verify_router.post("/v1/access_token")
+def issue_access_token(data : dict, expires_delta : timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(ZoneInfo("Asia/Seoul")) + expires_delta
+    else:
+        expire = datetime.now(ZoneInfo("Asia/Seoul")) + timedelta(minutes=15)
+    to_encode.update({"exp" : expire}) # 인증 만료 시간
+    encode_jwt = jwt.encode(to_encode, os.getenv('ACCESS_SECRET_KEY'), os.getenv('ALGORITHM'))
+    return encode_jwt  
+
+def authentication_user(db, session_id : str):
+    user = get_user(db, session_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Invalid Session_ID")
+
+
+# 사용자 정보(UUID, 이름, 포지션(0=부원, 1=랩장)) 가져오기
+@verify_router.get("/v1/userinfo")
+def get_user(uuid, name, db: Session = Depends(get_db)):
     try:
         members = db.query(OsMember).all()
         if not members:
